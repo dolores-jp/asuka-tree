a57d522ead1bbd57018b98878b9a013bf31fde5a
dolores <sosid000@gmail.com>
2026-01-02 16:35:33 +0900

    outlinedText

---
+++ AsukaTree/OutlinedText.cs
@@ -0,0 +1,133 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace AsukaTree
+{
+    /// <summary>
+    /// 文字を「塗り(Fill) + 縁(Stroke)」で描画する軽量コントロール。
+    /// </summary>
+    public sealed class OutlinedText : FrameworkElement
+    {
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register(nameof(Text), typeof(string), typeof(OutlinedText),
+                new FrameworkPropertyMetadata("", FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsRender));
+
+        public static readonly DependencyProperty FontSizeProperty =
+            TextElement.FontSizeProperty.AddOwner(typeof(OutlinedText),
+                new FrameworkPropertyMetadata(14.0, FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsRender));
+
+        public static readonly DependencyProperty FontFamilyProperty =
+            TextElement.FontFamilyProperty.AddOwner(typeof(OutlinedText),
+                new FrameworkPropertyMetadata(SystemFonts.MessageFontFamily, FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsRender));
+
+        public static readonly DependencyProperty FontWeightProperty =
+            TextElement.FontWeightProperty.AddOwner(typeof(OutlinedText),
+                new FrameworkPropertyMetadata(FontWeights.SemiBold, FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsRender));
+
+        public static readonly DependencyProperty ForegroundProperty =
+            DependencyProperty.Register(nameof(Foreground), typeof(Brush), typeof(OutlinedText),
+                new FrameworkPropertyMetadata(Brushes.Black, FrameworkPropertyMetadataOptions.AffectsRender));
+
+        public static readonly DependencyProperty StrokeProperty =
+            DependencyProperty.Register(nameof(Stroke), typeof(Brush), typeof(OutlinedText),
+                new FrameworkPropertyMetadata(Brushes.White, FrameworkPropertyMetadataOptions.AffectsRender));
+
+        public static readonly DependencyProperty StrokeThicknessProperty =
+            DependencyProperty.Register(nameof(StrokeThickness), typeof(double), typeof(OutlinedText),
+                new FrameworkPropertyMetadata(2.0, FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsRender));
+
+        public static readonly DependencyProperty PaddingProperty =
+            DependencyProperty.Register(nameof(Padding), typeof(Thickness), typeof(OutlinedText),
+                new FrameworkPropertyMetadata(new Thickness(0), FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsRender));
+
+        public string Text
+        {
+            get => (string)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+
+        public double FontSize
+        {
+            get => (double)GetValue(FontSizeProperty);
+            set => SetValue(FontSizeProperty, value);
+        }
+
+        public FontFamily FontFamily
+        {
+            get => (FontFamily)GetValue(FontFamilyProperty);
+            set => SetValue(FontFamilyProperty, value);
+        }
+
+        public FontWeight FontWeight
+        {
+            get => (FontWeight)GetValue(FontWeightProperty);
+            set => SetValue(FontWeightProperty, value);
+        }
+
+        public Brush Foreground
+        {
+            get => (Brush)GetValue(ForegroundProperty);
+            set => SetValue(ForegroundProperty, value);
+        }
+
+        public Brush Stroke
+        {
+            get => (Brush)GetValue(StrokeProperty);
+            set => SetValue(StrokeProperty, value);
+        }
+
+        public double StrokeThickness
+        {
+            get => (double)GetValue(StrokeThicknessProperty);
+            set => SetValue(StrokeThicknessProperty, value);
+        }
+
+        public Thickness Padding
+        {
+            get => (Thickness)GetValue(PaddingProperty);
+            set => SetValue(PaddingProperty, value);
+        }
+
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            var ft = CreateFormattedText();
+            // Padding を加味
+            return new Size(ft.Width + Padding.Left + Padding.Right, ft.Height + Padding.Top + Padding.Bottom);
+        }
+
+        protected override void OnRender(DrawingContext dc)
+        {
+            var ft = CreateFormattedText();
+
+            var origin = new Point(Padding.Left, Padding.Top);
+
+            // まず縁取り（ストローク）
+            var pen = new Pen(Stroke, StrokeThickness)
+            {
+                LineJoin = PenLineJoin.Round
+            };
+            var geo = ft.BuildGeometry(origin);
+            dc.DrawGeometry(null, pen, geo);
+
+            // 次に塗り（中身）
+            dc.DrawText(ft, origin);
+        }
+
+        private FormattedText CreateFormattedText()
+        {
+            // WPFのDPI対応
+            var dpi = VisualTreeHelper.GetDpi(this);
+
+            return new FormattedText(
+                Text ?? "",
+                CultureInfo.CurrentUICulture,
+                FlowDirection.LeftToRight,
+                new Typeface(FontFamily, FontStyles.Normal, FontWeight, FontStretches.Normal),
+                FontSize,
+                Foreground,
+                dpi.PixelsPerDip);
+        }
+    }
+}
